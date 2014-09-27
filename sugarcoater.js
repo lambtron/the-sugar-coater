ca12538c25d047b44d189aa5befa1363a0c05ce6
Andy Jiang <andyjiang@gmail.com>
2014-09-27 14:50:11 -0700

    added training, removed appearance

--- sugarcoater.js
+++ sugarcoater.js
@@ -3,7 +3,7 @@
 (function () {
 
 	var natural = require('natural');
-	var appearanceClassifier = new natural.BayesClassifier();
+	var officeClassifier = new natural.BayesClassifier();
 	var genderClassifier = new natural.BayesClassifier();
 	var responses = [];
 	var _ = require('underscore');
@@ -12,7 +12,7 @@
 		getMessage: function getMessage (input) {
 			// see what message it is.
 			var gender = getGender(input);	// dude, chick
-			var appearance = getAppearance(input); // appearance, nonappearance
+			var office = getOffice(input); // office, nonoffice
 
 			// search through responses.
 			// iterate through responses array
@@ -20,7 +20,7 @@
 			var potentialResponses = [];
 			for (var i = 0; i < responses.length; i++) {
 				if (responses[i].tags.indexOf(gender) > 0 ||
-					responses[i].tags.indexOf(appearance)) {
+					responses[i].tags.indexOf(office)) {
 					potentialResponses.push(responses[i].msg);
 				}
 			}
@@ -35,85 +35,109 @@
 	initializeResponses();
 
 	function trainSugarCoater () {
-	  appearanceClassifier.addDocument('', 'appearance');
-	  appearanceClassifier.addDocument('', 'not appearance');
-	  genderClassifier.addDocument('', 'dude');
-	  genderClassifier.addDocument('', 'chick');
+		var chick = [
+			"Die in a fire, fucking bitch!",
+			"Suck your own clit, cuntface.",
+			"Lobotomized professional victim, quit crying.",
+			"Keep complaining, slutface.  Nobody cares."
+		];
+
+		var dude = [
+			"Go to hell, you douchebag tool.",
+			"Hey, assclown, fall on a dildo.",
+			"Go to hell, numbnuts.",
+			"Hey, fuckface, clueless dickwad, loser.",
+			"Choke on a tampon, shithead.",
+			"Die slowly, motherfucker.",
+			"Hey, human hard-on, all of humanity is ashamed of you.",
+			"Fuck off, motherfucker."
+		];
+
+		var office = [
+			"Quit putting us to sleep, you boring, lazy fuck.",
+			"Everytime I see you, I pray for layoffs, asshole.",
+			"When are they going to fire you, waste of space."
+		];
+
+	  officeClassifier.addDocument(office, 'office');
+	  officeClassifier.addDocument('', 'notoffice');
+	  genderClassifier.addDocument(dude, 'dude');
+	  genderClassifier.addDocument(chick, 'chick');
 	}
 
 	function initializeResponses () {
 	  responses = [
 	    {
 	    	msg: "I love your shabby chic aesthetic!",
-	    	tags: ["appearance", "chick"]
+	    	tags: ["chick"]
 	    },
 	    {
 	    	msg: "Maybe you can take me thrift shopping sometime? You're so good at it.",
-	    	tags: ["appearance", "chick"]
+	    	tags: ["chick"]
 	    },
 	    {
 	    	msg: "It's great that you wore that. You are so comfortable in your own skin.",
-	    	tags: ["appearance"]
+	    	tags: ["chick"]
 	    },
 	    {
 	    	msg: "You just never let working with smart people intimidate you. I think that's so brave.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "I admire you for not giving in to the pressure to botox.",
-	    	tags: ["appearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "You're only ___?  You seem so mature for your age.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "You watch a lot of reality tv, right?  Maybe you \"can help me understand this...",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "You are so knowledgeable about reality TV and pop culture.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "I defer to your experience.  Of course, you are much closer to these things than I am.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "Was that blouse another street find?  Your dedication to recycling is so inspiring. I love how you are saving the planet one outfit at a time.",
-	    	tags: ["appearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "My cousin is a little slow. You know the one. He went to your college, I think?",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "My cousin, you know, the slow one? He has really unique taste. Can you help me with gift ideas for him?",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "You just don't care what other people think. I really admire that about you.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "It's so cool how you don't care what poeple think of you.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "You're so good at not letting what other people say about you bother you. I really admire that.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "I love how you just don't care.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "Dude, that's cool that you decided to date a smart girl.  Everybody's so hung up on looks.",
-	    	tags: ["nonappearance", "dude"]
+	    	tags: ["dude"]
 	    },
 	    {
 	    	msg: "Are you an only child?",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "Are you an actress?",
@@ -125,15 +149,15 @@
 	    },
 	    {
 	    	msg: "It's so great that you have so much time to spend on Facebook. I always see you on there.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "It's great how you are willing to stretch yourself and do things that don't come naturally to you.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "We all admire how hard you try.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "When are you due?!",
@@ -145,7 +169,7 @@
 	    },
 	    {
 	    	msg: "It's great that you posted that selfie. I love how unvain you are.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "Wow, you really remind me of my mother. (Leave hanging and unclarified the question of whether your mother is an unspeakably heinous human being.)",
@@ -161,41 +185,41 @@
 	    },
 	    {
 	    	msg: "This looks good. I don't know what they were talking about.",
-	    	tags: ["appearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "Are you tired? You look tired.",
-	    	tags: ["appearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "Give candy bar twelve pack as gift.",
-	    	tags: ["appearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "Give diet candy as gift",
-	    	tags: ["appearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "Walk or drive very slowly in front of person. Smile and waive when they pass.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "Regift gift back to person who gave it to you...in different wrapping paper.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "Regift something you received from this person to a third person...in their presence.",
-	    	tags: ["nonappearance"]
+	    	tags: ["chick", "dude"]
 	    },
 	    {
 	    	msg: "Start offering candy or desert and then stop midway.  \"Oh, I'm sorry. You're dieting, right? Nevermind.\"",
-	    	tags: ["appearance"]
+	    	tags: ["chick", "dude"]
 	    }
 	  ];
 	}
 
-	function getAppearance (line) {
-	  return appearanceClassifier.classify(line);
+	function getOffice (line) {
+	  return officeClassifier.classify(line);
 	}
 
 	function getGender (line) {
