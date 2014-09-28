98e07308affd244a237b1e5e14d51b53c85e5671
Tira Odhner <atiraodhner@gmail.com>
2014-09-27 17:52:06 -0700

    hardcode responses

--- routes.js
+++ routes.js
@@ -3,12 +3,26 @@
 (function() {
   // Helper libraries. =========================================================
   var sugarcoater = require('./sugarcoater.js');
+  var _ = require("underscore");
 
   // Public functions. =========================================================
   module.exports = function (app) {
     // API Routes ==============================================================
     app.post('/api/message', function (req, res) {
-      res.send({message: sugarcoater.getMessage(req.body.message)}, 200);
+
+      var message = (req.body.message);
+      var responseMessage = sugarcoater.getMessage(message);
+console.log("mesage", message, message.indexOf("clown") );
+      if (message.indexOf("in a fire") > -1 ){
+        responseMessage = "It's so brave of you to age naturally.";
+      }
+      if( message.indexOf("clown") > -1) {
+        responseMessage = "My cousin is a little slow. You know him, right? I thihnk you two went to college together?";
+      }
+      if(message.indexOf("nerdy") > -1){
+        responseMessage = "Good job attempting to be funny, everyone. You really tried."
+      }
+      res.send({message: responseMessage}, 200);
     });
 
     // Application routes ======================================================
